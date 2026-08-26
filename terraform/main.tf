13f38d91e9b56aeeb3fe06b57cd3c7293510b595
Debasis Das <dasdebasis250@gmail.com>
2026-08-26 18:11:27 +0530

    final

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,7 +83,7 @@ resource "aws_security_group" "project_security_group" {
 }
 
 resource "aws_instance" "project_instance" {
-    ami = "ami-0259ab82584019511" # Amazon Linux 2 AMI
+    ami = "ami-0226d82b291a8629d" # Amazon Linux 2 AMI
     instance_type = "t2.medium"
     subnet_id = aws_subnet.project_subnet_public_1.id
     vpc_security_group_ids = [aws_security_group.project_security_group.id]
